68c628e28c7ad906779869ea8e21ad8c205f9af2
Ankur Kothiwal <ankur.kothiwal99@gmail.com>
2019-11-18 23:03:59 +0530

    adds more opcodes functions

--- chip8.cpp
+++ chip8.cpp
@@ -23,62 +23,76 @@ unsigned char font_set[80]{
     0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
-chip8::chip8() {
+chip8::chip8()
+{
   pc = 0x200;
   opcode = 0;
   I = 0;
   sp = 0;
 
-  for (int i = 0; i < 64 * 32; i++) {
+  for (int i = 0; i < 64 * 32; i++)
+  {
     gfx[i] = 0; // clear the screen
   }
 
-  for (int i = 0; i < 16; i++) {
+  for (int i = 0; i < 16; i++)
+  {
     stack[i] = 0; // clear the stack
   }
 
-  for (int i = 0; i < 16; i++) {
+  for (int i = 0; i < 16; i++)
+  {
     key[i] = V[i] = 0; // clear the registers
   }
 
   for (int i = 0; i < 4096; ++i)
     memory[i] = 0; // clear memory
 
-  for (int i = 0; i < 80; i++) {
+  for (int i = 0; i < 80; i++)
+  {
     // load fontset
     memory[i] = font_set[i]; // The first 80 places are filled with the font set
   }
 }
 
-void chip8::load_program(char filepath[]) {
+void chip8::load_program(char filepath[])
+{
   streampos size;
   char *memblock;
 
   ifstream program;
   program.open(filepath, ios::in | ios::binary | ios::ate);
-  if (program.is_open()) {
+  if (program.is_open())
+  {
     size =
         program.tellg(); // size of the file, as the file is opened in ios::ate
     program.seekg(0, ios::beg);
     memblock = new char[size];
     program.read(memblock, size);
-    if ((4096 - 512) > size) {
-      for (int i = 0; i < size; i++) {
+    if ((4096 - 512) > size)
+    {
+      for (int i = 0; i < size; i++)
+      {
         memory[i + 512] = memblock[i];
       }
-    } else
+    }
+    else
       cout << "ROM is too big!!";
     program.close();
     delete (memblock);
-  } else
+  }
+  else
     cout << "Unable to open file";
 }
 
-int chip8::emulate_cycle() {
+int chip8::emulate_cycle()
+{
   opcode = memory[pc] << 8 | memory[pc + 1];
-  switch (opcode & 0xF000) {
+  switch (opcode & 0xF000)
+  {
   case 0x0000:
-    switch (opcode & 0x000F) {
+    switch (opcode & 0x000F)
+    {
     case 0x0000: // 0x00E0: clears the screen
       for (int i = 0; i < 2064; i++)
         gfx[i] = 0x0;
@@ -131,24 +145,25 @@ int chip8::emulate_cycle() {
     pc += 2;
     break;
   case 0x8000:
-    switch (opcode & 0x000F) {
+    switch (opcode & 0x000F)
+    {
     case 0x0000: // 8XY0: Sets VX to the value of VY
       V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
       pc += 2;
       break;
     case 0x0001: // 8XY1: Sets VX to VX or VY
       V[(opcode & 0x0F00) >> 8] =
-          V[(opcode & 0x0F00) >> 8] | V[(opcode & 0x00F0) >> 4];
+          V[(opcode & 0x0F00) >> 8] |= V[(opcode & 0x00F0) >> 4];
       pc += 2;
       break;
     case 0x0002: // 8XY2: Sets VX to VX and VY
       V[opcode & 0x0F00 >> 8] =
-          V[opcode & 0x0F00 >> 8] & V[opcode & 0x00F0 >> 4];
+          V[opcode & 0x0F00 >> 8] &= V[opcode & 0x00F0 >> 4];
       pc += 2;
       break;
     case 0x0003: // 8XY3: Sets VX to VX xor VY
       V[opcode & 0x0F00 >> 8] =
-          V[opcode & 0x0F00 >> 8] ^ V[opcode & 0x00F0 >> 4];
+          V[opcode & 0x0F00 >> 8] ^= V[opcode & 0x00F0 >> 4];
       pc += 2;
       break;
     case 0x0004: // 8XY4: Adds VY to VX. VF is set to 1 when there's a carry,
@@ -175,8 +190,13 @@ int chip8::emulate_cycle() {
       V[opcode & 0x0F00 >> 8] <<= 1;
       pc += 2;
       break;
-    case 0x0007:
-      // TODO: 8XY7
+    case 0x0007: // 0x8XY7: Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't
+      if (V[(opcode & 0x0F00) >> 8] > V[(opcode & 0x00F0) >> 4])
+        V[0xF] = 0;
+      else
+        V[0xF] = 1;
+      V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4] - V[(opcode & 0x0F00) >> 8];
+      pc += 2;
       break;
     case 0x000E: // 8XYE: Stores the most significant bit of VX in VF and then
                  // shifts VX to the left by 1.
@@ -219,20 +239,30 @@ int chip8::emulate_cycle() {
     // TO DO
     break;
   case 0xE000:
-    switch (opcode & 0x00FF) {
-    case 0x009E:
-      // TODO: EX9E
+    switch (opcode & 0x00FF)
+    {
+    case 0x009E: // EX9E: Skips the next instruction if the key stored in VX is pressed (Usually the next instruction is a jump to skip a code block)
+      if (key[V[(opcode & 0x0F00) >> 8]] != 0)
+        pc += 4;
+      else
+        pc += 2;
       break;
-    case 0x00A1:
-      // TODO: EXA1
+
+    case 0x00A1: // EXA1: Skips the next instruction if the key stored in VX isn't pressed. (Usually the next instruction is a jump to skip a code block)
+      if (key[V[(opcode & 0x0F00) >> 8]] == 0)
+        pc += 4;
+      else
+        pc += 2;
       break;
+
     default:
       cout << "Invalid opcode";
       break;
     }
     break;
   case 0xF000:
-    switch (opcode & 0x0FF) {
+    switch (opcode & 0x0FF)
+    {
     case 0x0007: // FX07: Sets VX to the value of the delay timer.
       V[opcode & 0x0F00 >> 8] = delay_timer;
       pc += 2;
@@ -252,18 +282,29 @@ int chip8::emulate_cycle() {
       I += V[opcode & 0x0F00 >> 8];
       pc += 2;
       break;
-    case 0x0029:
-      // TODO: FX29
+    case 0x0029: // FX29: Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font
+      I = V[(opcode & 0x0F00) >> 8] * 0x5;
+      pc += 2;
       break;
-    case 0x0033:
-      // TODO: FX33
+    case 0x0033: // FX33: Stores the Binary-coded decimal representation of VX at the addresses I, I plus 1, and I plus 2
+      memory[I] = V[(opcode & 0x0F00) >> 8] / 100;
+      memory[I + 1] = (V[(opcode & 0x0F00) >> 8] / 10) % 10;
+      memory[I + 2] = (V[(opcode & 0x0F00) >> 8] % 100) % 10;
+      pc += 2;
       break;
-    case 0x0055:
-      // TODO: FX55
+    case 0x0055: // FX55: Stores V0 to VX in memory starting at address I
+      for (int i = 0; i <= ((opcode & 0x0F00) >> 8); ++i)
+        memory[I + i] = V[i];
+      I += ((opcode & 0x0F00) >> 8) + 1;
+      pc += 2;
       break;
-    case 0x0065:
-      // TODO: FX65
+    case 0x0065: // FX65: Fills V0 to VX with values from memory starting at address I
+      for (int i = 0; i <= ((opcode & 0x0F00) >> 8); ++i)
+        V[i] = memory[I + i];
+      I += ((opcode & 0x0F00) >> 8) + 1;
+      pc += 2;
       break;
+
     default:
       cout << "Invaild opcode";
       break;
@@ -275,7 +316,8 @@ int chip8::emulate_cycle() {
   if (delay_timer > 0)
     --delay_timer;
 
-  if (sound_timer > 0) {
+  if (sound_timer > 0)
+  {
     if (sound_timer == 1)
       printf("BEEP!\n");
     --sound_timer;
